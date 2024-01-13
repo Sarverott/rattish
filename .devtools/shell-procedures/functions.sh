e722a0c497cbeca68abce05a1efb8eaa7b651b93
Sett Sarverott <sarverott@gmail.com>
2024-01-13 22:47:08 +0100

    autoupdate message generator functions

--- .devtools/shell-procedures/functions.sh
+++ .devtools/shell-procedures/functions.sh
@@ -15,11 +15,32 @@ sync_repo() {
   git submodule update
 }
 
+get_update_body() {
+  echo "current version: $(git rev-list --count $(git branch --show-current))"
+  echo "remote version: $(git rev-list --count origin/main)"
+}
+
+get_update_title() {
+  echo "$GIT_CURRENT_USER@$1->PULL_REQUEST : version $2 = $(date +%s)"
+}
+
 update_and_upload_repo() {
+
+  UPDATE_CHECK_LOCAL=$(git rev-list --count $(git branch --show-current))
+  UPDATE_CHECK_REMOTE=$(git rev-list --count origin/main)
+
+  if [[ ! $UPDATE_CHECK_LOCAL == $UPDATE_CHECK_REMOTE ]]; then
+    sync_repo $1
+
+  fi
+
   cd ../$1
   git add *
-  read COMMIT_INFO
-  git commit -m "$GIT_CURRENT_USER@$1 : autoupdating-devtools = $(date +%s)"
+  git commit -m
   git push
-  gh pr create --title "$GIT_CURRENT_USER@$1 : autoupdating-devtools = $(date +%s)" --body "$GIT_CURRENT_USER@$1 : autoupdating-devtools = $(date +%s)"
+
+  PULL_REQUEST_CONTENT=$(git diff main remotes/origin/main --shortstat)
+
+  git diff main remotes/origin/main --numstat
+  gh pr create --title "$GIT_CURRENT_USER@$1 : autoupdating $1 = $(date +%s)" --body " \n "
 }
